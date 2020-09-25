33d15ea508b710fe4a0a13f92f5cf2eb149accfd
narayanrachit <narayanrachit@gmail.com>
2020-09-25 23:48:05 +0530

    Updates

---
+++ precache-manifest.aac68d9cbb9183bd4669dc3adfcbfa86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf0a7d424cd0973ac74d155a170baf1",
+    "url": "/TodoReact/index.html"
+  },
+  {
+    "revision": "7144933e7c94ee4e1216",
+    "url": "/TodoReact/static/css/main.6297a899.chunk.css"
+  },
+  {
+    "revision": "fa7d14e11782cca41eb9",
+    "url": "/TodoReact/static/js/2.1cc1485b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TodoReact/static/js/2.1cc1485b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7144933e7c94ee4e1216",
+    "url": "/TodoReact/static/js/main.8599a15a.chunk.js"
+  },
+  {
+    "revision": "8a9a74e593898b3e5363",
+    "url": "/TodoReact/static/js/runtime-main.a23dc319.js"
+  }
+]);
